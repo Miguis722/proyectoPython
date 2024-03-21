cfc1a3fad451a6935a4ee8bfd3f059a1670fcabf
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-21 13:48:06 -0500

    Ponemos a prueba el menú y añadimos las distintas opciones a seleccionar. Además de que añadimos documentación al código para entenderlo despues.

--- main.py
+++ main.py
@@ -7,3 +7,36 @@ import json
 import requests
 #Empezamos importando las primeras funciones que necesitaremos en un futuro
 #Empezaremos haciendo un menú de bienvenida a los usuarios y más adelante podremos importar el resto de procesos.
+
+
+
+
+
+def menuPrincipal():
+    while True:
+        #CLS se usa en vez del CLEAR, debido a que uso Windows y no Linux.
+        os.system("cls")
+        #Sacamos el diseño del Menú principal
+        # https://patorjk.com/software/taag/#p=display&h=2&v=2&f=Small%20Keyboard&t=Menu%20Principal
+
+        print("""
+              
+ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ ____ ____ ____ ____ 
+||M |||e |||n |||u |||       |||P |||r |||i |||n |||c |||i |||p |||a |||l ||
+||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
+
+              
+              1. ACTIVOS
+              2. PERSONAL
+              3. ZONAS
+              4. ASIGNACION DE ACTIVOS
+              5. REPORTES
+              6. MOVIMIENTO DE ACTIVOS
+              7. SALIR
+
+              """)
+
+menuPrincipal()
+
+print(menuPrincipal)
