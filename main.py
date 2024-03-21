e388b910c9518faf52a4c1fe56583dffa90e52d1
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-21 13:20:34 -0500

    Creación del archivo de MAIN, gitnore y las carpetas donde iran cada uno de los procesos y/o información necesaria durante el desarrollo

---
+++ main.py
@@ -0,0 +1,9 @@
+from tabulate import tabulate
+
+import re
+import os
+import sys
+import json
+import requests
+#Empezamos importando las primeras funciones que necesitaremos en un futuro
+#Empezaremos haciendo un menú de bienvenida a los usuarios y más adelante podremos importar el resto de procesos.
