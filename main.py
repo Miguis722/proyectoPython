01fab621b21d601b3f3d558ecc283d376bbc7191
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-21 15:21:57 -0500

    Inicio del desarrollo del menú interactivo añadiendo de documentación

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import requests
 
 
 
-
-def menuPrincipal():
+if (__name__ == '__main__'):
+#def menuPrincipal():
     while True:
         #CLS se usa en vez del CLEAR, debido a que uso Windows y no Linux.
         os.system("cls")
@@ -20,13 +20,11 @@ def menuPrincipal():
         # https://patorjk.com/software/taag/#p=display&h=2&v=2&f=Small%20Keyboard&t=Menu%20Principal
 
         print("""
-              
- ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ ____ ____ ____ ____ 
-||M |||e |||n |||u |||       |||P |||r |||i |||n |||c |||i |||p |||a |||l ||
-||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__|||__|||__|||__||
-|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
+                                MENÚ PRINCIPAL
 
+                    SISTEMA G&C DE INVENTARIO CAMPUSLANDS
               
+        OPCIONES  
               1. ACTIVOS
               2. PERSONAL
               3. ZONAS
@@ -34,9 +32,14 @@ def menuPrincipal():
               5. REPORTES
               6. MOVIMIENTO DE ACTIVOS
               7. SALIR
-
               """)
-
-menuPrincipal()
-
-print(menuPrincipal)
+#Es importante poner un tiempo final para que no siga apareciendo en bucle.
+#Ya que puede hacer que el dispositivo se trabe o deje de funcionar optimamente.
+        opcion = input("\nSeleccione una de las opciones: ")
+        #Pedimos al usuario ingresar un número para escoger la opción que desea del menú
+        if re.match(r'^[1-7]$', opcion) is not None:
+            #Con esta validación vamos a comprobar que el número que ingrese se encuentre
+            #Dentro del parametro de 1 a 7. Además, de que en caso de que ingrese un 
+            #número distinto a los posibles, volvera a sacar el mismo menú.
+            opcion = int(opcion)  # Lo convertimos a un número entero
+            #Empezamos a meter los condicionales para el menú.
