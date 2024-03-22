961024821c857f5cc3cebc23098a6ea5446a72ea
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-22 07:37:10 -0500

    Desarrollamos los SUBMENÚS de ACTIVOS, PERSONAL y ZONAS. En ellos pusimos las condiciones de que solo podemos escoger de 1 hasta 5, si escoge un número distinto dentro de ese rango, volvera a aparecer ese mismo menú hasta que se ingrese un número valido.

--- Modules/getPersonal.py
+++ Modules/getPersonal.py
@@ -9,7 +9,8 @@ def menu():
                 |M|E|N|U| |-| |P|E|R|S|O|N|A|L|
                 +-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+
 
-                
+              
+            OPCIONES
                 1. AGREGAR
                 2. EDITAR
                 3. ELIMINAR
@@ -17,3 +18,22 @@ def menu():
                 5. REGRESAR AL MENU PRINCIPAL
 
               """)
+        opcion = input("\nSeleccione una de las opciones: ")
+        #Pedimos al usuario ingresar un número para escoger la opción que desea del menú de PERSONAL
+        if re.match(r'^[1-5]$', opcion) is not None:
+            #Con esta validación vamos a comprobar que el número que ingrese se encuentre
+            #Dentro del parametro de 1 a 7. Además, de que en caso de que ingrese un 
+            #número distinto a los posibles, volvera a sacar el mismo menú.
+            opcion = int(opcion)  # Lo convertimos a un número entero
+            #Empezamos a meter los condicionales para el menú.
+        if (opcion == 1):
+            #Agregar
+        elif (opcion == 2):
+            #Editar
+        elif (opcion == 3):
+            #Eliminar
+        elif (opcion == 4):
+            #Buscar
+        elif(opcion == 5):
+            break
+        #Volver al menú principal
